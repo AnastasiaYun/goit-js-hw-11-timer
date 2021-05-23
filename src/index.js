6e299dff5532f6ba6ba9784827933ff8b27484ab
AnastasiaYun <tigrawom@gmail.com>
2021-05-23 23:03:35 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import './sass/main.scss';
+class CountdownTimer{
+
+    constructor({selector,targetDate}) {
+      this.selector = selector;    
+      this.targetDate = targetDate;
+      this.referens =  document.querySelector(this.selector);
+      this.days = document.querySelector(`${this.selector} [data-value="days"]`);
+      this.hours = document.querySelector(`${this.selector} [data-value="hours"]`)
+      this.mins = document.querySelector(`${this.selector} [data-value="mins"]`)
+      this.secs = document.querySelector(`${this.selector} [data-value="secs"]`)
+  
+    };
+      
+    start() {
+      this.referens.style.display = 'inline-flex';
+      setInterval(() => {
+        const currentTime = Date.now();
+      console.log(this.targetDate.getTime());
+        const deltaTime = this.targetDate.getTime() - currentTime;
+        const time = this.getTimeComponents(deltaTime);
+      console.log(time)
+        this.updateTime(time);
+      }, 1000);
+    };
+  
+     updateTime({ days, hours, mins, secs }) {
+      this.days.textContent = `${days}`;
+      this.hours.textContent = `${hours}`;
+      this.mins.textContent = `${mins}`;
+      this.secs.textContent = `${secs}`; 
+    };
+    
+    getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  
+    return {days, hours, mins, secs}
+    };
+    
+    pad(value) {
+      return String(value).padStart(2, '0');
+    }
+  };
+  
+  const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jun 18, 2021'),
+  });
+  
+  countdownTimer.start()
